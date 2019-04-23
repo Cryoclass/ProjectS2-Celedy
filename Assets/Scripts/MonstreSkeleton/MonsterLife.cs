d1b4792b570c1db6710b5a5b4a06ede26ae67d19
Célian Crepin <celian.crepin@gmail.com>
2019-04-23 23:10:13 +0200

    avant le rendu

---
+++ Assets/Scripts/MonstreSkeleton/MonsterLife.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterLife : MonoBehaviour
+{
+    public float Life;
+
+    public void damaged(int i)
+    {
+        Life -= i;
+        if (Life < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
