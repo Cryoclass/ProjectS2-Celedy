876d722fb8fa0f60e741fb4fdabd69e7ac53b585
OmbreAstrale91 <49863003+OmbreAstrale91@users.noreply.github.com>
2019-04-23 15:18:41 +0200

    sound

---
+++ Assets/Scripts/Menu/MainMenu/TransitionScenesMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionScenesMusic : MonoBehaviour
+{
+    
+    private bool isMusic;
+
+
+    
+    void Awake()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+        
+        DontDestroyOnLoad(transform.gameObject);
+    }
+}
