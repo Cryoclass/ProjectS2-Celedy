0c173b758daee6e2a7a7e6e622d42a6ba7125887
Célian Crepin <celian.crepin@gmail.com>
2019-04-23 18:10:54 +0200

    Plein de trucs (tir)

---
+++ Assets/Scripts/Spells/Weapon.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public void Shooti(GameObject gam)
+    {
+        Instantiate(gam, transform.position, transform.rotation);
+    }
+}
