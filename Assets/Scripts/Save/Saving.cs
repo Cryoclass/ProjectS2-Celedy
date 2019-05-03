7cd35e00b3c3a7039cbc865689f958fbfa5edc16
OmbreAstrale91 <49863003+OmbreAstrale91@users.noreply.github.com>
2019-05-03 22:41:07 +0200

    début sauvegarde

---
+++ Assets/Scripts/Save/Saving.cs
@@ -0,0 +1,12 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class Saving
+{
+    public static void SavePlayerDate(YaelleData Yaelle)
+    {
+        
+    }
+}*/
---
+++ Assets/Scripts/Save/YaelleData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class YaelleData : MonoBehaviour
+{
+    //sauvegarde la progression du joueur
+    public int CurrentHealth;
+    public int CurrentMaxHeart;
+    public float[] position;
+
+    public YaelleData(Ya_Health Yaelle)
+    {
+        CurrentHealth = Yaelle.CurrentHealth;
+        CurrentMaxHeart = Yaelle.CurrentMaxHeart;
+        position = new float[3];
+        position[0] = Yaelle.transform.position.x;
+        position[1] = Yaelle.transform.position.y;
+        position[2] = Yaelle.transform.position.z;
+        
+        
+
+    }
+}
