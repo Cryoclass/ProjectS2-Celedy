0de5842154f4042a2cb42d1546f89748bd6464b2
OmbreAstrale91 <49863003+OmbreAstrale91@users.noreply.github.com>
2019-05-06 20:28:03 +0200

    save script

--- Assets/Scripts/Save/Saving.cs
+++ Assets/Scripts/Save/Saving.cs
@@ -1,12 +1,41 @@
-﻿/*using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using System.Runtime.Serialization.Formatters.Binary;
 
 public static class Saving
 {
-    public static void SavePlayerDate(YaelleData Yaelle)
+    public static void SavePlayerData(Ya_Health Yaelle)
     {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/Yaelle.txt";
         
+        FileStream stream = new FileStream(path, FileMode.Create); //créer un fichier de sauvegarde dans le chemin spécifié
+        YaelleData data = new YaelleData(Yaelle);
+        
+        formatter.Serialize(stream, data); //écrit dans la sauvegarde les informations sur Yaelle
+        stream.Close();
     }
-}*/
+    
+    
+    //va chercher la sauvegarde de yaelle
+    public static YaelleData LoadingData()
+    {
+        string path = Application.persistentDataPath + "/Yaelle.txt";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);//ouvre le fichier de sauvegarde
+
+            YaelleData data = (YaelleData) formatter.Deserialize(stream); //
+            stream.Close(); //j'ai pas vrmt compris pourquoi faut obligatoirement le fermer, mais ca met des erreurs quand on le ferme pas
+
+            return data;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
