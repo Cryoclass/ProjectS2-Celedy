fff9d757331a9073f3d84bbcae9885b9fb4a0676
Edouard Farrando <edouard.farrando@epita.fr>
2019-06-01 20:16:43 +0200

    Taurus à peu pres fini

--- Assets/Scripts/Torus/TorusIA.cs
+++ Assets/Scripts/Torus/TorusIA.cs
@@ -12,8 +12,11 @@ public enum States
     Charging
 }
 
-public class TorusIA : MonoBehaviour
+public class TorusIA : Take_damage
 {
+    private Color Col;
+    private PortSens lastWallSens;
+    private Animator anim;// position: 1 = face; 2 = dos ; 3 = side;
     SpriteRenderer[] ChidrenSprites;
 
     private Transform PlayerPos;
@@ -27,11 +30,19 @@ public class TorusIA : MonoBehaviour
     public GameObject ShockWave;
 
     public GameObject Crack;
+    private PortSens WallSens;
+    public GameObject leftpoint;
+    public GameObject rightpoint;
+    public int Life;
     // Start is called before the first frame update
     void Start()
     {
         ChidrenSprites = gameObject.GetComponentsInChildren<SpriteRenderer>();
+        Col = ChidrenSprites[0].color;
+        lastWallSens = PortSens.Null;
+        anim = GetComponent<Animator>();
         PlayerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        GetTarget();
     }
 
     // Update is called once per frame
@@ -39,43 +50,102 @@ public class TorusIA : MonoBehaviour
     {
         foreach (SpriteRenderer SprRend in ChidrenSprites)
         {
-            SprRend.color += new Color(255,255,255);
+            SprRend.color = new Color(1f,1f,1f);
         }
     }
 
     private void MakeProgresivelyRed()
     {
         state = States.Preparing;
-        foreach (SpriteRenderer SprRend in ChidrenSprites)
+        Col.b -= 0.1f;
+        Col.g -= 0.1f;
+        foreach(SpriteRenderer spr in ChidrenSprites)
         {
-            SprRend.color += new Color(5,0,0);
+            spr.color = Col;
         }
-
-        if (ChidrenSprites.Length < 1 || ChidrenSprites[0].color.r >= 255)
+        if (Col.b <= 0)
         {
             CancelInvoke("MakeProgresivelyRed");
             Charge();
         }
     }
 
+    private void GetTarget()
+    {
+        Target = PlayerPos.position - transform.position;
+        Target.Normalize();
+        if (Target.x > Target.y)
+        {
+            if (-Target.x < Target.y)
+            {
+                anim.SetInteger("Position",3);
+                transform.rotation = Quaternion.Euler(0f,0f,0f);
+            }
+            else
+            {
+                anim.SetInteger("Position", 1);
+                transform.rotation = Quaternion.Euler(0f,0,0f);
+            }
+        }
+        else
+        {
+            if (-Target.x < Target.y)
+            {
+                anim.SetInteger("Position",3);
+                transform.rotation = Quaternion.Euler(0f,180f,0f);            
+            }
+            else
+            {
+                anim.SetInteger("Position", 2);
+                transform.rotation = Quaternion.Euler(0f,0f,0f);
+            }
+        }
+        InvokeRepeating("MakeProgresivelyRed",0f,0.3f);
+    }
+    
     private void Charge()
     {
         MakeMeWhite();
         state = States.Charging;
-        Target = PlayerPos.position - transform.position;
-        Target.Normalize();
         rgb.velocity = Target * ChargeSpeed;
+        anim.SetBool("Charging",true);
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         CollidTag = other.gameObject.tag;
         switch (CollidTag)
         {
             case "Wall":
             {
-                rgb.velocity = new Vector2(0, 0);
-                Explosion();
+                WallSens = other.GetComponent<WallS>().sens;
+                if (lastWallSens != WallSens)
+                {
+                    transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+                    rgb.velocity = new Vector3(0, 0,0);
+                    if (WallSens == PortSens.Bot || WallSens == PortSens.Top)
+                    {
+                        Explosion();
+                    }
+                    else if (WallSens == PortSens.Left)
+                    {
+                        ExplosionRight();
+                    }
+                    else if (WallSens == PortSens.Right)
+                    {
+                        ExplosionLeft();
+                    }
+                    else
+                    {
+                        ExplosionSides();
+                    }
+                    GetTarget();
+                    anim.SetBool("Shock",true);
+                    lastWallSens = other.GetComponent<WallS>().sens;
+                    Life--;
+                    if(Life <= 0)
+                        Destroy(gameObject);
+                }
                 break;
             }
             case "Player":
@@ -86,21 +156,74 @@ public class TorusIA : MonoBehaviour
         }
     }
 
+    private void ExplosionRight()
+    {
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 90));
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 120));
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 150));
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 180));
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 210));
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 240));
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 270));
+        
+        Instantiate(Crack,transform.position,transform.rotation);
+    }
+    private void ExplosionLeft()
+    {
+        Instantiate(ShockWave, leftpoint.transform.position,Quaternion.Euler(0, 0, 0));
+        Instantiate(ShockWave, leftpoint.transform.position,Quaternion.Euler(0, 0, 30));
+        Instantiate(ShockWave, leftpoint.transform.position,Quaternion.Euler(0, 0, 60));
+        
+        Instantiate(ShockWave, leftpoint.transform.position,Quaternion.Euler(0, 0, 210));
+        Instantiate(ShockWave, leftpoint.transform.position,Quaternion.Euler(0, 0, 240));
+        Instantiate(ShockWave, leftpoint.transform.position,Quaternion.Euler(0, 0, 270));
+        Instantiate(ShockWave, leftpoint.transform.position,Quaternion.Euler(0, 0, 300));
+        Instantiate(ShockWave, leftpoint.transform.position,Quaternion.Euler(0, 0, 330));
+        
+        Instantiate(Crack,transform.position,transform.rotation);
+    }
     private void Explosion()
     {
-        Instantiate(ShockWave, transform.position, Quaternion.Euler(0f,0f,0f), transform);
-        Instantiate(ShockWave, transform.position, Quaternion.Euler(0f,0f,30), transform);
-        Instantiate(ShockWave, transform.position, Quaternion.Euler(0f,0f,60), transform);
-        Instantiate(ShockWave, transform.position, Quaternion.Euler(0f,0f,90), transform);
-        Instantiate(ShockWave, transform.position, Quaternion.Euler(0f,0f,120), transform);
-        Instantiate(ShockWave, transform.position, Quaternion.Euler(0f,0f,150), transform);
-        Instantiate(ShockWave, transform.position, Quaternion.Euler(0f,0f,180), transform);
-        Instantiate(ShockWave, transform.position, Quaternion.Euler(0f,0f,210), transform);
-        Instantiate(ShockWave, transform.position, Quaternion.Euler(0f,0f,240), transform);
-        Instantiate(ShockWave, transform.position, Quaternion.Euler(0f,0f,270), transform);
-        Instantiate(ShockWave, transform.position, Quaternion.Euler(0f,0f,300), transform);
-        Instantiate(ShockWave, transform.position, Quaternion.Euler(0f,0f,330), transform);
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 0));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 30));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 60));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 90));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 120));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 150));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 180));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 210));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 240));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 270));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 300));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 330));
+        
+        
+        Instantiate(Crack,transform.position,transform.rotation);
+    }
 
-        Instantiate(Crack);
+    private void ExplosionSides()
+    {
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 0));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 30));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 60));
+        
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 210));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 240));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 270));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 300));
+        Instantiate(ShockWave, transform.position,Quaternion.Euler(0, 0, 330));
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 90));
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 120));
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 150));
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 180));
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 210));
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 240));
+        Instantiate(ShockWave, rightpoint.transform.position,Quaternion.Euler(0, 0, 270));
+        
+        Instantiate(Crack,transform.position,transform.rotation);
+    }
+    public override void InflictDamage(int i)
+    {
+        
     }
 }
