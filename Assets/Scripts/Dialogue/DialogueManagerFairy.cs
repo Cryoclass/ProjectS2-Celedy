26d412567a6522a5e4a8e6491f28daf7e2a47f3d
Sophian Berbagui <sophian.berbagui@epita.fr>
2019-06-03 18:38:00 +0200

    hahaha

--- Assets/Scripts/Dialogue/DialogueManagerFairy.cs
+++ Assets/Scripts/Dialogue/DialogueManagerFairy.cs
@@ -19,7 +19,19 @@ public class DialogueManagerFairy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string initial = "Qu'est ce que tu me veux toi?! + Euuh bonjour petite fée! + Détend toi parcontre! \n Ah oui hihi bonjour jeune garçon qui cherche une fée dans sa quête à la c**pour sauver une princesse qui l'a friendzone de toute façon! + elle m'a pas.. elle m'a pas friendzone... + Tu vas redescendre d'un ton jeune fille! \n Qu'est ce qu'il me veut le merdeux? Eh !Ecoute!retourne te taper un génocide en pensant que t'as raison c'est pas mes affaires!! + Toujours mieux que de rester dans une pièce toute la journée a fumer sa propre poudre + Pourquoi tu hurles comme ça? \n Bouffon... *End!* + + \n JE SUIS PAS UNE PETITE FILLE!+ Euh ? + Euh ? \n Je...euh.. (il semblerait que vous l'ayez vexée...) *End!* \n JE HURLE PAS! + OoooOooooOhhHhHHHH!!!! + JE VAIS TE CALMER MOI! \n Il semblerait que les cris aient attirés des monstres... *New Ally* \n Il semblerait que les cris aient attirés des monstres... *New Ally* \n + + \n + + \n (Votre cri d'animal gênerait n'importe qui...elle vous ignore) + *End !* + *End !* \n C'EST CE QU'ON VA VOIR! (Il semblerait que vos cris aient attirés des monstres...) + *New Ally* + *New Ally*";
+        string initial = "Qu'est ce que tu me veux toi?! + Euuh bonjour petite fée! + Détend toi parcontre! " +
+                         "\n Ah oui hihi bonjour jeune garçon qui cherche une fée dans sa quête à la c**pour sauver une princesse qui l'a friendzone de toute façon! + elle m'a pas.. elle m'a pas friendzone... + Tu vas redescendre d'un ton jeune fille! " +
+                         "\n Qu'est ce qu'il me veut le merdeux? Eh !Ecoute!retourne te taper un génocide en pensant que t'as raison c'est pas mes affaires!! + Toujours mieux que de rester dans une pièce toute la journée a fumer sa propre poudre + Pourquoi tu hurles comme ça? " +
+                         "\n Bouffon... *End!* + + " +
+                         "\n JE SUIS PAS UNE PETITE FILLE!+ Euh ? + Euh ? " +
+                         "\n Je...euh.. (il semblerait que vous l'ayez vexée...) *End!* " +
+                         "\n JE HURLE PAS! + OoooOooooOhhHhHHHH!!!! + JE VAIS TE CALMER MOI! " +
+                         "\n Il semblerait que les cris aient attirés des monstres... *New Ally* " +
+                         "\n Il semblerait que les cris aient attirés des monstres... *New Ally* " +
+                         "\n + + " +
+                         "\n + + " +
+                         "\n (Votre cri d'animal gênerait n'importe qui...elle vous ignore) + *End !* + *End !* " +
+                         "\n C'EST CE QU'ON VA VOIR! (Il semblerait que vos cris aient attirés des monstres...) +  + ";
 
         i = 0;
         sentences = new List<string>();
